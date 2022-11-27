40dd1d2d3e782fabec146daae2694cf5fb9297bf
ubaldo21093 <112136996+ubaldo21093@users.noreply.github.com>
2022-11-26 20:42:16 -0700

    Add files via upload

--- PokemonCollection.java
+++ PokemonCollection.java
@@ -19,22 +19,66 @@ public class PokemonCollection extends BST<Pokemon> {
 
     // TODO #6: add the given pokemon card to the collection; if type1 is defined, check if the given pokemon card has the same type1
     public void add(final Pokemon pokemon) {
+        if (isEmpty()){
+            root = new BSTNode<>(pokemon);
+        }else if (pokemon.getType1().equals("")){
+            add(pokemon);
+        } else if (pokemon.getType1().compareTo(this.type1) == 0) {
+            add(pokemon);
+        }
 
     }
+
 
     // TODO #7: similar to BST's searchRecursively, returning a Pokemon reference (instead of true/false); return null if the pokemon card is not found
     private Pokemon searchRecursively(final BSTNode<Pokemon> current, final Pokemon value) {
-        return null;
+        if (value.compareTo(current.getValue()) == 0)
+            return current.getValue();
+        else if (value.compareTo(current.getValue()) < 0) {
+            if (current.getLeft() == null)
+                return null;
+            else
+                return searchRecursively(current.getLeft(), value);
+        } else {
+            if (current.getRight() == null)
+                return null;
+            else
+                return searchRecursively(current.getRight(), value);
+        }
     }
 
-    // TODO #8: similar to BST's search; however, this method takes the name of a Pokemon and it should return avPokemon reference (instead of true/false); return null if the pokemon card is not
+    // TODO #8: similar to BST's search; however, this method takes the name of a Pokemon and it should return a Pokemon reference (instead of true/false); return null if the pokemon card is not
     public Pokemon search(final String name) {
-        return null;
+        if (isEmpty())
+            return null;
+        else
+            return searchRecursively(root, new Pokemon(name));
     }
 
     // TODO #9: return a string representation of a pokemon collection according to specifications in README file
     @Override
     public String toString() {
-        return "";
+        if (!type1.equals("")){
+            return "Welcome to my Pokemon Cards Collection.\nThere are" + size()  + "Pokemon cards"
+                    + "\nThis collection has Pokemon cards of type1='" + type1 + "' only."
+                    + "\nThe pokemon cards are stored in a BST for optimized search."
+                    + "\nThe current height of the BST is " + height();
+        }
+        return "Welcome to my Pokemon Cards Collection.\nThere are" + size()  + "Pokemon cards"
+                + "\nThe pokemon cards are stored in a BST for optimized search."
+                + "\nThe current height of the BST is " + height();
     }
 }
+
+//  Without Type1:
+//    Welcome to my Pokemon Cards Collection.
+//        There are 800 pokemon cards.
+//        The pokemon cards are stored in a BST for optimized search.
+//        The current height of the BST is 23.
+
+// With Type1
+//    Welcome to my Pokemon Cards Collection.
+//        This collection has pokemon cards of type1='Water' only.
+//        There are 112 pokemon cards.
+//        The pokemon cards are stored in a BST for optimized search.
+//        The current height of the BST is 15.
--- PokemonDriver.java
+++ PokemonDriver.java
@@ -7,12 +7,28 @@
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
+
 
 public class PokemonDriver {
 
     // TODO #10: open the CSV filename and use it to create a return a PokemonCollection of the given type1
     static PokemonCollection load(String filename, String type1) throws FileNotFoundException {
+        String pokemonFile = "pokemon.csv";
+        FileInputStream input = new FileInputStream(pokemonFile);
+        String name;String type;String type2;
+        int total; int hp; int attack; int defense; int spAttack; int spDefense; int speed; int generation;
+        boolean legendary;
+
+        try {
+            // 1,Bulbasaur,Grass,Poison,318,45,49,49,65,65,45,1,False
+
+
+            Pokemon current = new Pokemon(name, type, type2, total, hp, attack, defense, spAttack, spDefense, speed, generation, legendary);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        while ()
         return null;
     }
 
@@ -21,3 +37,34 @@ public class PokemonDriver {
         
     }
 }
+
+
+
+//import java.io.FileInputStream;
+//
+//public class Main {
+//
+//    public static void main(String args[]) {
+//
+//        try {
+//            FileInputStream input = new FileInputStream("input.txt");
+//
+//            System.out.println("Data in the file: ");
+//
+//            // Reads the first byte
+//            int i = input.read();
+//
+//            while(i != -1) {
+//                System.out.print((char)i);
+//
+//                // Reads next byte from the file
+//                i = input.read();
+//            }
+//            input.close();
+//        }
+//
+//        catch(Exception e) {
+//            e.getStackTrace();
+//        }
+//    }
+//}
